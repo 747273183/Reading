43743604db064de8b22cf1f2e4b214b2518e24dc
森林之王 <747273183@qq.com>
2019-08-15 14:41:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/reading/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.reading;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public  static  void start(Activity activity)
+    {
+        Intent intent=new Intent(activity,MainActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+}
