f003468dcf8fea586e45cba9887e92e42c234251
森林之王 <747273183@qq.com>
2019-08-16 18:52:19 +0800

    5.下载保存书籍为.txt文件

--- app/src/main/java/com/example/reading/BaseLisViewAdapter.java
+++ app/src/main/java/com/example/reading/BaseLisViewAdapter.java
@@ -1,6 +1,8 @@
 package com.example.reading;
 
 import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +10,20 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+
+import java.io.File;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
 
 public class BaseLisViewAdapter extends BaseAdapter {
+    private static final String TAG = "BaseLisViewAdapter";
 
     private Context context;
     private List<BookListResult.Book> books;
@@ -57,8 +66,62 @@ public class BaseLisViewAdapter extends BaseAdapter {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        BookListResult.Book book = books.get(position);
+        final BookListResult.Book book = books.get(position);
         viewHolder.tvBookname.setText(book.getBookname());
+
+        //点击下载
+        final ViewHolder finalViewHolder = viewHolder;
+        //根据文件是否存在来决定按钮上的显示的文本(点击下载/开始阅读)
+        final String filePath =context.getFilesDir()+ File.separator+ book.getBookname() + ".txt";
+        File file=new File(filePath);
+        if (file.exists())
+        {
+            finalViewHolder.btnDownload.setText("开始阅读");
+        }
+        else
+        {
+            finalViewHolder.btnDownload.setText("点击下载");
+        }
+        viewHolder.btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String btnText = finalViewHolder.btnDownload.getText().toString();
+                if ("点击下载".equals(btnText))
+                {
+                    AsyncHttpClient client=new AsyncHttpClient();
+                    //设置不压缩,否则无法检测文件的大小,就不能显示正确的下载进度
+
+                    client.addHeader("Accept-Encoding","identity");
+                    client.get(book.getBookfile(), new FileAsyncHttpResponseHandler(new File(filePath)) {
+                        //下载失败
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                            finalViewHolder.btnDownload.setText("下载失败");
+                        }
+
+                        //下载成功
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, File file) {
+                            finalViewHolder.btnDownload.setText("开始阅读");
+                            Log.d(TAG, "onSuccess: "+file.getAbsolutePath());
+                        }
+
+                        //下载进度
+                        @Override
+                        public void onProgress(long bytesWritten, long totalSize) {
+                            super.onProgress(bytesWritten, totalSize);
+                            finalViewHolder.btnDownload.setText((bytesWritten*100/totalSize)+"%");
+                        }
+                    });
+                }
+                else if ("开始阅读".equals(btnText))
+                {
+                        //todo 开始阅读
+                    Toast.makeText(context, "开始阅读", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
         return convertView;
     }
--- app/src/main/java/com/example/reading/SplashActivity.java
+++ app/src/main/java/com/example/reading/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.example.reading;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
@@ -11,6 +15,7 @@ import butterknife.OnClick;
 
 public class SplashActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE = 1101;
     public static int time = 3000;
     public static final int STEP_TIME = 1000;
     @BindView(R.id.btn_skip)
@@ -30,11 +35,14 @@ public class SplashActivity extends AppCompatActivity {
     };
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
+
+
 
         countDownTimer.start();
     }
