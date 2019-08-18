2485494a6a3a83fc5d3777f8d18e8868a46ba137
森林之王 <747273183@qq.com>
2019-08-18 10:26:58 +0800

    8.添加书签和读取书签

--- app/src/main/java/com/example/reading/ReadingActivity.java
+++ app/src/main/java/com/example/reading/ReadingActivity.java
@@ -39,6 +39,9 @@ public class ReadingActivity extends AppCompatActivity {
 
     private List<String> strings=new ArrayList<>();
     private SettingRecyclerAdapter settingRecyclerAdapter;
+    public int currentLength;
+    public String filePath;
+    private BookPageBezierHelper bookPageBezierHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +55,41 @@ public class ReadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reading);
         ButterKnife.bind(this);
 
+        //打开书
+        openBook(0);
+        //添加设置菜单
+        addSettingMenu();
+
+
+    }
+
+    private void addSettingMenu() {
+        //设置recyclerView的适配器
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        strings.add("添加书签");
+        strings.add("读取书签");
+        strings.add("设置背景");
+        strings.add("语音朗读");
+        strings.add("跳转进度");
+
+        settingRecyclerAdapter=new SettingRecyclerAdapter(this,strings);
+
+        recyclerView.setAdapter(settingRecyclerAdapter);
+
+
+        //显示/隐藏 设置view(1.添加书签 2.读取书签 3.设置背景  4.语音朗读 5.跳转进度)
+        bookPageView.setOnUserNeedSettingListener(new BookPageView.OnUserNeedSettingListener() {
+            @Override
+            public void onUserNeedSetting() {
+                settingView.setVisibility(settingView.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+            }
+        });
+    }
+
+    public void openBook(int progress) {
         //设置翻页的宽高
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -59,7 +97,7 @@ public class ReadingActivity extends AppCompatActivity {
         int height = displayMetrics.heightPixels;
 
         //设置翻页
-        BookPageBezierHelper bookPageBezierHelper = new BookPageBezierHelper(width, height);
+        bookPageBezierHelper = new BookPageBezierHelper(width, height,progress);
         bookPageView.setBookPageBezierHelper(bookPageBezierHelper);
 
         //设置当前页 下一页
@@ -73,54 +111,30 @@ public class ReadingActivity extends AppCompatActivity {
         bookPageBezierHelper.setOnProgressChangedListener(new BookPageBezierHelper.OnProgressChangedListener() {
             @Override
             public void setProgress(int currentLength, int totalLength) {
+                ReadingActivity.this.currentLength=currentLength;
                 String format = String.format("%.4f", currentLength * 100.0000 / totalLength);
                 tvProgress.setText(format + "%");
             }
         });
 
-
         //加载书内容
-        String filePath = getIntent().getStringExtra("filePath");
+        filePath = getIntent().getStringExtra("filePath");
         try {
             bookPageBezierHelper.openBook(filePath);//打开
             bookPageBezierHelper.draw(new Canvas(currentPageBitMap));//显示第一页
+            bookPageView.invalidate();//重绘
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //设置recyclerView的适配器
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-
-        strings.add("添加书签");
-        strings.add("读取书签");
-        strings.add("设置背景");
-        strings.add("语音朗读");
-        strings.add("跳转进度");
-
-       settingRecyclerAdapter=new SettingRecyclerAdapter(this,strings);
-
-       recyclerView.setAdapter(settingRecyclerAdapter);
-
-
-        //显示/隐藏 设置view(1.添加书签 2.读取书签 3.设置背景  4.语音朗读 5.跳转进度)
-        bookPageView.setOnUserNeedSettingListener(new BookPageView.OnUserNeedSettingListener() {
-            @Override
-            public void onUserNeedSetting() {
-                settingView.setVisibility(settingView.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
-            }
-        });
-
-
-
-
     }
+
+
 
     public static void start(Context activity, String filePath) {
         Intent intent = new Intent(activity, ReadingActivity.class);
         intent.putExtra("filePath", filePath);
         activity.startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/reading/adapter/SettingRecyclerAdapter.java
+++ app/src/main/java/com/example/reading/adapter/SettingRecyclerAdapter.java
@@ -1,14 +1,18 @@
 package com.example.reading.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.reading.R;
+import com.example.reading.ReadingActivity;
+import com.example.reading.view.BookPageBezierHelper;
 
 import java.util.List;
 
@@ -38,9 +42,44 @@ public class SettingRecyclerAdapter extends RecyclerView.Adapter<SettingRecycler
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, final int i) {
         String s = stringList.get(i);
         myViewHolder.tvName.setText(s);
+
+        //显示/隐藏 设置view(1.添加书签 2.读取书签 3.设置背景  4.语音朗读 5.跳转进度)
+        myViewHolder.tvName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReadingActivity readingActivity=   (ReadingActivity)context;
+                SharedPreferences sharedPreferences=context.getSharedPreferences("bookmarks",Context.MODE_PRIVATE);
+                switch (i)
+                {
+                    case 0:
+                        SharedPreferences.Editor edit = sharedPreferences.edit();
+                        //设置某本书读取的进度
+                        edit.putInt(readingActivity.filePath,readingActivity.currentLength);
+                        edit.commit();
+                        Toast.makeText(readingActivity, "添加书签成功"+readingActivity.currentLength, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+
+                        int progress = sharedPreferences.getInt(readingActivity.filePath, 0);
+                        Toast.makeText(context, "读取书签"+progress, Toast.LENGTH_SHORT).show();
+                        readingActivity.openBook(progress);
+
+                        break;
+                    case 2:
+                        Toast.makeText(context, "设置背景", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(context, "语音朗读", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(context, "跳转进度", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 
     @Override
